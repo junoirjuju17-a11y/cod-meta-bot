16cb32176c5616a6f51244bc678a76470ae04b09
junoirjuju17-a11y <junoirjuju17@gmail.com>
2026-07-06 17:18:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,36 @@ import os
 import discord
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-guild = client.guilds[0]
-print(f"Serveur : {guild.name} ({guild.id})")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 
-for ch in guild.text_channels:
-    print(f"{ch.name} -> {ch.id}")
-
-channel = guild.get_channel(CHANNEL_ID)
 intents = discord.Intents.default()
+intents.guilds = True
 
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f"Connecté : {client.user}")
+    print(f"✅ Connecté : {client.user}")
 
-    channel = await client.fetch_channel(CHANNEL_ID)
-    await channel.send("✅ Le bot fonctionne !")
+    print("Serveurs :")
+    for guild in client.guilds:
+        print(f"- {guild.name} ({guild.id})")
 
-    await client.close()
+        print("Salons :")
+        for ch in guild.text_channels:
+            print(f"  {ch.name} -> {ch.id}")
+
+    try:
+        channel = client.get_channel(CHANNEL_ID)
+
+        if channel is None:
+            print("❌ Salon introuvable")
+            return
+
+        await channel.send("✅ Test du bot réussi !")
+        print("✅ Message envoyé")
+
+    except Exception as e:
+        print(e)
 
 client.run(TOKEN)
