726e51d2e6e1219817674a50a6fa61380e42e106
junoirjuju17-a11y <junoirjuju17@gmail.com>
2026-07-06 17:47:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import discord
+from discord.ext import tasks
+from scraper import get_meta
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
@@ -8,18 +10,54 @@ intents = discord.Intents.default()
 
 client = discord.Client(intents=intents)
 
+last_meta = None
+
+
 @client.event
 async def on_ready():
-    print(f"Connecté : {client.user}")
+    print(f"✅ Connecté : {client.user}")
+
+    if not check_meta.is_running():
+        check_meta.start()
+
+
+@tasks.loop(minutes=10)
+async def check_meta():
+    global last_meta
 
     channel = client.get_channel(CHANNEL_ID)
 
     if channel is None:
-        print("Salon introuvable")
+        print("❌ Salon introuvable")
         return
 
-    await channel.send("✅ Le bot fonctionne !")
+    try:
+        weapons = await get_meta()
 
-    print("Message envoyé !")
+        if not weapons:
+            print("Aucune arme trouvée.")
+            return
+
+        current = weapons[0]
+
+        if last_meta is None:
+            last_meta = current
+            print(f"Méta actuelle : {current}")
+            return
+
+        if current != last_meta:
+            last_meta = current
+
+            await channel.send(
+                f"🔥 **Nouvelle arme META détectée !**\n\n"
+                f"**{current}**\n\n"
+                f"https://wzstats.gg/fr"
+            )
+
+            print("Nouvelle méta envoyée.")
+
+    except Exception as e:
+        print(e)
+
 
 client.run(TOKEN)
